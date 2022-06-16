e4403016f919c9bbe80abde36b7985d709c55db0
Sean <seancze@gmail.com>
2022-06-16 12:42:50 +0800

    Remove duplicate code

--- Cohort Execises/CE5/node-app-esc.js
+++ Cohort Execises/CE5/node-app-esc.js
@@ -2,40 +2,33 @@
 // where your node app starts
 
 // init project
-const express = require('express');
+const express = require("express");
 const app = express();
 
-
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('.'));
+app.use(express.static("."));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/index.html');
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/index.html");
   response.end();
 });
 
-app.get('/about', function(request, response) {
-  response.sendFile(__dirname + '/about.html');
+app.get("/about", function (request, response) {
+  response.sendFile(__dirname + "/about.html");
   response.end();
 });
 
-app.get('/contact', function(request, response) {
-  response.sendFile(__dirname + '/contact.html');
-  response.end();
-});
-
-app.get('/contact', function(request, response) {
-  response.sendFile(__dirname + '/contact.html');
+app.get("/contact", function (request, response) {
+  response.sendFile(__dirname + "/contact.html");
   response.end();
 });
 
 // listen for requests :)
-const listener = app.listen("3000", function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen("3000", function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
 
 app.use((request, response, next) => {
-  response.status(404).sendFile(__dirname + '/404_error.html');
-})
-
+  response.status(404).sendFile(__dirname + "/404_error.html");
+});
